3d046999349485ea0d702b24ed30da321267eb04
Laryssa <laryssam444@gmail.com>
2025-09-15 20:19:14 -0300

    Atualização da Frase

--- frase.py
+++ frase.py
@@ -1,2 +1,15 @@
-print("teste")
-print("hello, wordl")
+def gerar_frase():
+    
+    frase_escolhida = input("Olá. Digite uma frase que você goste:")
+    
+    palavras = frase_escolhida.slip()
+    
+    numero_de_palavras = len(palavras)
+    
+    palavras_invertidas = palavras[::-1]
+    
+    frase_escolhida = ''.join(palavras_invertidas)
+    
+gerar_frase()
+
+print(f )
