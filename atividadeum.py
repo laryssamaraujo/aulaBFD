3241de1560eb721a8a27571099352ad5ccc5faf9
Laryssa <laryssam444@gmail.com>
2025-09-29 21:04:02 -0300

    Atualização

---
+++ atividadeum.py
@@ -0,0 +1,14 @@
+nome = str(input("Olá! Qual o seu nome?"))
+
+idade = int(input("Quantos anos você tem?"))
+
+telefone = int(input("Qual o número do seu celular?"))
+
+cidade = str(input("Qual a sua cidade?"))
+
+endereço = str(input("Me fala o seu endereço completo?"))
+
+print(f"Olá, {nome}! É um prazer falar com você.")
+print(f"Você tem {idade} anos e seu número de telefone é {telefone}.")
+print(f"Vi aqui que você reside no município de {cidade} com endereço na {endereço}.")
+print("Obrigada!")
